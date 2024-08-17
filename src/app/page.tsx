54c17addfedb5214fb1664ef6089ae27f8bef718
cetina19 <cetina19@itu.edu.tr>
2024-08-17 18:05:49 +0300

    auth

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+export default async function Home() {
+  const session = await getServerSession();
+  return (
+    <>
+      getServerSession Result
+      {session?.user?.name ? (
+        <div>{session?.user?.name}</div>
+      ) : (
+        <div> Not Logged In </div>
+      )}
+    </>
+  );
+}
